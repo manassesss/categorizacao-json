bebd58bfcfb78766f5f24bcbd6e9cb945dd2f17a
manasses.silva <manasses3mm@gmail.com>
2021-08-16 11:53:06 -0300

    mudanças na categorização e arquivo final

--- Categorias.py
+++ Categorias.py
@@ -19,6 +19,9 @@ class Categoria (ABC):
     def classificarUnidade():
         pass
     
+    def classificarFinal():
+        pass
+    
 class Assistencia_Social(Categoria):
     def classificarClasse(self, dicionario):
         '''
@@ -29,14 +32,14 @@ class Assistencia_Social(Categoria):
         '''
         for i in dicionario:
             name = dicionario[i]['name'].split()
-            if ("CREAS" in name) or ("CRAS" in name) or ("Abrigo" in name) or ("CentroPOP" in name) or ("CentroDia" in name) or ("SEMCASPI" in name) or ("CentrodeConvivência" in name) or ("Conselho Tutelar" in name) or ("AssociaçãodosAmigosAutistas" in name) or ("LAC" in name) or ("ASA/" in name) or ("APAE" in name) or ("APADA" in name) or ("ACEP" in name):
+            if ("CREAS" in name) or ("CRAS" in name) or ("Abrigo" in name) or ("CentroPOP" in name) or ("CentroDia" in name) or ("SEMCASPI" in name) or ("CentrodeConvivência" in name) or ("Conselho Tutelar" in name) or ("AssociaçãodosAmigosAutistas" in name) or ("LAC" in name) or ("ASA/" in name) or ("APAE" in name) or ("APADA" in name) or ("ACEP" in name) in ("Casade" in name):
                 self.save[i] = dicionario[i]
                 self.save[i]['classe'] = "Assistencia Social"
     
     def classificarUnidade(self):
         '''
         Responsável por classificar os objetos do arquivo JSON em categorias. Neste caso nas categorias CREAS, 
-        CRAS, Abrigo, Centro POP, Centro Dia, Centro de Convivência, Conselho Tutelar, AMA, LAC, ASA, APAE, APADA, 
+        CRAS, Abrigo, Centro POP, Centro Dia, Centro de Convivência, Conselho Tutelar, AMA, LAC, ASA, APAE, APADA, Casa de 
         
         :parametro: Não tem parametro, ele utiliza uma variavel da classe (save) para salvar os objetos do arquivo 
                     JSON com as alterações feitas.
@@ -72,6 +75,11 @@ class Assistencia_Social(Categoria):
                 self.save[i]['unidade'] = 'ACEP'
             if "Libras" in name:
                 self.save[i]['unidade'] = 'Libras'
+            if "Casade" in name:
+               self.save[i]['unidade'] = 'Casa'
+    
+    def classificarFinal(self):
+        pass
 
 class Saude(Categoria):
     def classificarClasse(self, dicionario):
@@ -117,14 +125,14 @@ class Educacao(Categoria):
         '''
         for i in dicionario:
             name = dicionario[i]['name'].split()
-            if ("Escola" in name) or ("Biblioteca" in name) or ("CMEI" in name) or ("E.M." in name) or ("Creche" in name):
+            if ("Escola" in name) or ("Biblioteca" in name) or ("CMEI" in name) or ("E.M." in name) or ("Creche" in name) or ("CentrodeCapacitação" in name):
                 self.save[i] = dicionario[i]
                 self.save[i]['classe'] = "Educacao"
     
     def classificarUnidade(self):
         '''
         Responsável por classificar os objetos do arquivo JSON em categorias. Neste caso nas categorias Escola,
-        Biblioteca, CMEI, Creche.
+        Biblioteca, CMEI, Creche, Centro de Capacitação
         
         :parametro: Não tem parametro, ele utiliza uma variavel da classe (save) para salvar os objetos do arquivo 
                     JSON com as alterações feitas.
@@ -140,6 +148,8 @@ class Educacao(Categoria):
                 self.save[i]['unidade'] = 'CMEI'
             if "Creche" in name:
                 self.save[i]['unidade'] = 'Creche'
+            if "CentrodeCapacitação" in name:
+                self.save[i]['unidade'] = 'Centro de Capacitacao'
             
 
 class EsporteELazer(Categoria):
@@ -152,7 +162,7 @@ class EsporteELazer(Categoria):
         '''
         for i in dicionario:
             name = dicionario[i]['name'].split()
-            if ("Praca" in name) or ("Praça" in name) or ("Ginásio" in name) or ("CEU" in name) or ("Parque" in name) or ("CampoSociety" in name) or ("CampoOficial" in name) or ("AcademiadaTerceiraIdade" in name) or ("AcademiaaoArLivre" in name) or ("Quadra" in name):
+            if ("Praca" in name) or ("Praça" in name) or ("Ginásio" in name) or ("CEU" in name) or ("Parque" in name) or ("CampoSociety" in name) or ("CampoOficial" in name) or ("AcademiadaTerceiraIdade" in name) or ("AcademiaaoArLivre" in name) or ("Quadra" in name) or ("ComplexoEsportivo" in name) or ("Estádio" in name) or ("Skate" in name) or ("PistaOlímpica" in name):
                 self.save[i] = dicionario[i]
                 self.save[i]['classe'] = "Esporte e Lazer"
     
@@ -160,7 +170,7 @@ class EsporteELazer(Categoria):
         '''
         Responsável por classificar os objetos do arquivo JSON em categorias. Neste caso nas categorias Praça,
         Ginásio, CEU, Parque, Campo Society, Campo Oficial, Academia da Terceira Idade, Academia ao Ar Livre, 
-        Quadra
+        Quadra, Complexo Esportivo, Estádio, Pista de Skate, Pista Olímpica
         
         :parametro: Não tem parametro, ele utiliza uma variavel da classe (save) para salvar os objetos do arquivo 
                     JSON com as alterações feitas.
@@ -180,14 +190,22 @@ class EsporteELazer(Categoria):
                 self.save[i]['unidade'] = 'Campo Society'
             if "CampoOficial" in name:
                 self.save[i]['unidade'] = 'Campo Oficial'
-            if "CampoOficial" in name:
-                self.save[i]['unidade'] = 'Campo Oficial'
             if "AcademiadaTerceiraIdade" in name:
                 self.save[i]['unidade'] = 'Academia da Terceira Idade'
             if "AcademiaaoArLivre" in name:
                 self.save[i]['unidade'] = 'Academia ao Ar Livre'
             if "Quadra" in name:
                 self.save[i]['unidade'] = 'Quadra'
+            if "ComplexoEsportivo" in name:
+                self.save[i]['unidade'] = 'Complexo Esportivo'
+            if "Estádio" in name:
+                self.save[i]['unidade'] = 'Estadio'
+            if "Skate" in name:
+                self.save[i]['unidade'] = 'Pista de Skate'
+            if "PistaOlímpica" in name:
+                self.save[i]['unidade'] = 'Pista Olimpica'
+            
+            
             
 class Predios_Publicos(Categoria):
     def classificarClasse(self, dicionario):
@@ -199,7 +217,7 @@ class Predios_Publicos(Categoria):
         '''
         for i in dicionario:
             name = dicionario[i]['name'].split()
-            if ("SEMEC" in name) or ("SEMCOP" in name) or ("SEMAM" in name) or ("SEMA" in name) or ("PRODATER" in name) or ("SEMPLAN" in name)or ("PGM" in name) or ("IPMT" in name)or ("SEMDUH" in name)or ("SEMCASPI" in name)or ("SEMCOP" in name) or ("SEMDEC" in name)or ("SEMEST" in name) or ("SEMEL" in name) or ("SEMPOM" in name) or ("SEMJUV" in name) or ("STRANS" in name) or ("FMS" in name)or ("ARSETE" in name)or ("SDR" in name)or ("FMC" in name)or ("FWF" in name) or ("PrefeituradeTeresina" in name) or ("VicePrefeitura" in name) or ("SDU" in name) or("CAT" in name):
+            if ("SEMEC" in name) or ("SEMCOP" in name) or ("SEMAM" in name) or ("SEMA" in name) or ("PRODATER" in name) or ("SEMPLAN" in name)or ("PGM" in name) or ("IPMT" in name)or ("SEMDUH" in name)or ("SEMCASPI" in name)or ("SEMCOP" in name) or ("SEMDEC" in name)or ("SEMEST" in name) or ("SEMEL" in name) or ("SEMPOM" in name) or ("SEMJUV" in name) or ("STRANS" in name) or ("FMS" in name)or ("ARSETE" in name)or ("SDR" in name)or ("FMC" in name)or ("FWF" in name) or ("PrefeituradeTeresina" in name) or ("VicePrefeitura" in name) or ("SDU" in name) or("CAT" in name) or ("PredioAdministrativo" in name) or ("GerênciadeFarmâcia" in name) or ("CentrodeIniciação" in name):
                 self.save[i] = dicionario[i]
                 self.save[i]['classe'] = "Predios Publicos"
     
@@ -260,6 +278,17 @@ class Predios_Publicos(Categoria):
                 self.save[i]['unidade'] = 'SDU'
             if "CAT" in name:
                 self.save[i]['unidade'] = 'CAT'
+            if "SEMEC" in name or "Semec" in name:
+                self.save[i]['unidade'] = 'SEMEC'
+            if "CMAM" in name:
+                self.save[i]['unidade'] = 'CMAM'
+            if "PredioAdministrativo" in name:
+                self.save[i]['unidade'] = 'Predio Administrativo'
+            if "GerênciadeFarmâcia" in name:
+                self.save[i]['unidade'] = 'Gerencia de Farmacia'
+            if "CentrodeIniciação" in name:
+                self.save[i]['unidade'] = 'CIE'
+                
 class Meio_Ambiente(Categoria):
         def classificarClasse(self, dicionario):
             '''
@@ -270,7 +299,7 @@ class Meio_Ambiente(Categoria):
             '''
             for i in dicionario:
                 name = dicionario[i]['name'].split()
-                if ("PEV" in name) or ("PontodeRecebimentodeResíduo" in name) or ("EstaçãodeTransbordodeResíduosSólidos" in name) or ("HC" in name):
+                if ("PEV" in name) or ("PontodeRecebimentodeResíduo" in name) or ("EstaçãodeTransbordodeResíduosSólidos" in name) or ("HC" in name) or ("Viveiro" in name) or ("CampoAgricola" in name) or ("CampoAgrícola" in name):
                     self.save[i] = dicionario[i]
                     self.save[i]['classe'] = "Meio Ambiente"
         
@@ -293,6 +322,11 @@ class Meio_Ambiente(Categoria):
                     self.save[i]['unidade'] = 'Estação de Transbordo de Resíduos Sólidos'
                 if "HC" in name:
                     self.save[i]['unidade'] = 'Hortas Comunitárias'
+                if "Viveiro" in name:
+                    self.save[i]['unidade'] = 'Viveiro de Mudas'
+                if "CampoAgricola" in name or "CampoAgrícola" in name:
+                    self.save[i]['unidade'] = 'Campo Agricola'
+                
 
 class Saneamento(Categoria):
         def classificarClasse(self, dicionario):
@@ -324,6 +358,43 @@ class Saneamento(Categoria):
                 if "EstaçãoElevatória" in name:
                     self.save[i]['unidade'] = 'Estação Elevatória'
 
+class Cultura(Categoria):
+        def classificarClasse(self, dicionario):
+            '''
+            Responsável por classificar os objetos do arquivo JSON em classes. Neste caso na classe Cultura
+            
+            :parametro dicionario: objeto dict() que contém os objetos do arquivo JSON.
+            :retorno none: Não retorna nada ainda.
+            '''
+            for i in dicionario:
+                name = dicionario[i]['name'].split()
+                if ("Museu" in name) or ("CasadaCultura" in name) or ("Teatro" in name) or ("Galeria" in name) or ("Palácio" in name):
+                    self.save[i] = dicionario[i]
+                    self.save[i]['classe'] = "Cultura"
+        
+        def classificarUnidade(self):
+            '''
+            Responsável por classificar os objetos do arquivo JSON em categorias. Neste caso nas categorias Museu, Casa da Cultura,
+            Teatro, Galeira, Palacio
+            
+            :parametro: Não tem parametro, ele utiliza uma variavel da classe (save) para salvar os objetos do arquivo 
+                        JSON com as alterações feitas.
+            :retorno save: Retorna o o dicionario save com as informações novas
+            '''
+            for i in self.save:
+                name = self.save[i]['name'].split()
+                if "Museu" in name:
+                    self.save[i]['unidade'] = 'Museu'
+                if "CasadaCultura" in name:
+                    self.save[i]['unidade'] = 'Casa da Cultura'
+                if "Teatro" in name:
+                    self.save[i]['unidade'] = 'Teatro'
+                if "Galeria" in name:
+                    self.save[i]['unidade'] = 'Galeria'
+                if "Palácio" in name:
+                    self.save[i]['unidade'] = 'Palacio'
+                
+
 class Outros(Categoria):
         def classificarClasse(self, dicionario):
             '''
@@ -334,7 +405,7 @@ class Outros(Categoria):
             '''
             for i in dicionario:
                 name = dicionario[i]['name'].split()
-                if ("Cemitério" in name):
+                if ("Cemitério" in name) or ("Lavanderia" in name) or ("CentrodeProdução" in name) or ("Rotula" in name) or ("Rótula" in name) or ("PoloEmpresarial" in name) or ("Bicicletário" in name):
                     self.save[i] = dicionario[i]
                     self.save[i]['classe'] = "Outros"
         
@@ -351,4 +422,15 @@ class Outros(Categoria):
                 name = self.save[i]['name'].split()
                 if "Cemitério" in name:
                     self.save[i]['unidade'] = 'Cemitério'
+                if "Lavanderia" in name:
+                    self.save[i]['unidade'] = 'Lavanderia'
+                if "CentrodeProdução" in name:
+                    self.save[i]['unidade'] = 'Centro de Producao'
+                if "Rótula" in name:
+                    self.save[i]['unidade'] = 'Rotula'
+                if "PoloEmpresarial" in name:
+                    self.save[i]['unidade'] = 'Polo Empresarial'
+                if "Bicicletário" in name:
+                    self.save[i]['unidade'] = 'Bicicletário'
+
 
--- categorizacao.py
+++ categorizacao.py
@@ -8,7 +8,7 @@ from numpy import rint
 import pandas as pd
 
 #carregando  o arquivo .json
-with open("teresinense-digital-unidades_new-export.json", "r", encoding="utf8") as json_file:
+with open("final.json", "r", encoding="utf8") as json_file:
     u1 = json.load(json_file)
 
 #lista de chaves geradas pelo firebase anteriormente
@@ -18,7 +18,9 @@ u2 = {}
 #novas chaves
 cont = 0
 #teste para salvar
-save = {}
+save = {'Educacao': []}
+print(save)
+part = {'icon': '', 'lat': '', 'long': '', 'name': '', 'unidade': ''}
 #salvando as chaves geradas pelo firebase em na lista de chaves
 for i in u1:
     u1_keys.append(i)
@@ -28,14 +30,19 @@ for i in u1_keys:
     u2[cont] = u1[i]
     cont+=1
 
-for i in u2:
-    name = u2[i]['name'].split()
-    if ("CREAS" in name):
-         save[i] = u2[i]
 
-for i in save:
-    name = save[i]['name'].split()
-    if "CREAS" in name:
-        save[i]['categoria'] = 'CREAS'
-        
+
+for i in u2:
+    classe = u2[i]['classe'].split()
+    if ("Educacao" in classe):
+         part['icon'] = u2[i]['icon']
+         part['lat'] = u2[i]['lat']
+         part['long'] = u2[i]['long']
+         part['name'] = u2[i]['name']
+         part['unidade'] = u2[i]['unidade']
+         save['Educacao'].insert(i, part)
+
+with open("test.json", "w", encoding="utf8") as json_outfile:
+    json.dump(save, json_outfile, ensure_ascii=False,indent=4)
+
 print(save)
